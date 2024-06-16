6e17df049ce2e8456e6f1136340f46a55d0823c5
Natalia Borysowska-Ślęczka <natalia.borysowska@gmail.com>
2024-06-16 12:12:50 +0200

    Final version

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace kolko_i_krzyzyk
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblWho = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.resultO = new System.Windows.Forms.Label();
+            this.resultX = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn1.Location = new System.Drawing.Point(34, 64);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(150, 150);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn2.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn2.Location = new System.Drawing.Point(190, 64);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(150, 150);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn3.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn3.Location = new System.Drawing.Point(346, 64);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(150, 150);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn4.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn4.Location = new System.Drawing.Point(34, 220);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(150, 150);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn5.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn5.Location = new System.Drawing.Point(190, 220);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(150, 150);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn6.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn6.Location = new System.Drawing.Point(346, 220);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(150, 150);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn7.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn7.Location = new System.Drawing.Point(34, 376);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(150, 150);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn8.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn8.Location = new System.Drawing.Point(190, 376);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(150, 150);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn9.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn9.Location = new System.Drawing.Point(346, 376);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(150, 150);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(583, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 46);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Player :";
+            // 
+            // lblWho
+            // 
+            this.lblWho.AutoSize = true;
+            this.lblWho.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblWho.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblWho.Location = new System.Drawing.Point(724, 64);
+            this.lblWho.Name = "lblWho";
+            this.lblWho.Size = new System.Drawing.Size(46, 46);
+            this.lblWho.TabIndex = 10;
+            this.lblWho.Text = "O";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(632, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 38);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "RESULT";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(533, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 46);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "O :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(735, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 46);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "X :";
+            // 
+            // resultO
+            // 
+            this.resultO.AutoSize = true;
+            this.resultO.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.resultO.Location = new System.Drawing.Point(601, 270);
+            this.resultO.Name = "resultO";
+            this.resultO.Size = new System.Drawing.Size(38, 46);
+            this.resultO.TabIndex = 14;
+            this.resultO.Text = "0";
+            // 
+            // resultX
+            // 
+            this.resultX.AutoSize = true;
+            this.resultX.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.resultX.Location = new System.Drawing.Point(797, 270);
+            this.resultX.Name = "resultX";
+            this.resultX.Size = new System.Drawing.Size(38, 46);
+            this.resultX.TabIndex = 15;
+            this.resultX.Text = "0";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnRestart.Location = new System.Drawing.Point(525, 459);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(319, 67);
+            this.btnRestart.TabIndex = 16;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(882, 603);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.resultX);
+            this.Controls.Add(this.resultO);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblWho);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kółko i krzyżyk";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Label label1;
+        private Label lblWho;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label resultO;
+        private Label resultX;
+        private Button btnRestart;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+namespace kolko_i_krzyzyk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool player1 = true;
+        int move = 0;
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            Restart();
+            resultO.Text = "0";
+            resultX.Text = "0";
+            lblWho.Text = "O";
+            player1 = true;
+        }
+
+        private void Restart()
+        {
+            move = 0;
+            Button[] all = new Button[9];   // tablica 9 przycisków
+
+            /*
+            all[0] = btn1;
+            all[1] = btn2;
+            all[2] = btn3;
+            all[3] = btn4;
+            all[4] = btn5;
+            all[5] = btn6;
+            all[6] = btn7;
+            all[7] = btn8;
+            all[8] = btn9;
+            */
+
+            for (int i = 0; i < all.Length; i++)
+            {
+                all[i] = (Button)this.Controls.Find("btn" + (i + 1), true)[0];
+            }
+
+            foreach (Button b in all)
+            {
+                b.Enabled = true;
+                b.Text = "";
+            }
+        }
+        private void Check()
+        {
+            if (btn1.Text != "" && btn1.Text == btn2.Text && btn2.Text == btn3.Text)
+            {
+                Win();
+            }
+            else if (btn4.Text != "" && btn4.Text == btn5.Text && btn5.Text == btn6.Text)
+            {
+                Win();
+            }
+            else if (btn7.Text != "" && btn7.Text == btn8.Text && btn8.Text == btn9.Text)
+            {
+                Win();
+            }
+            else if (btn1.Text != "" && btn1.Text == btn4.Text && btn4.Text == btn7.Text)
+            {
+                Win();
+            }
+            else if (btn2.Text != "" && btn2.Text == btn5.Text && btn5.Text == btn8.Text)
+            {
+                Win();
+            }
+            else if (btn3.Text != "" && btn3.Text == btn6.Text && btn6.Text == btn9.Text)
+            {
+                Win();
+            }
+            else if (move == 9)
+            {
+                MessageBox.Show("REMIS", "Koniec gry", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Restart();
+            }
+        }
+
+        private void Win()
+        {
+            MessageBox.Show("WYGRYWA GRACZ: " + (player1 ? "O" : "X"), "Wygrana!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            if (player1)
+            {
+                resultO.Text = ((int.Parse(resultO.Text)) + 1).ToString();
+            }
+            else
+            {
+                resultX.Text = ((int.Parse(resultX.Text)) + 1).ToString();
+            }
+            Restart();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            move++;
+            ((Button)sender).Text = player1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+
+            if (move >= 5)
+            {
+                Check();
+            }
+
+            player1 = !player1;     // zmiana gracza po wykonaniu ruchu
+            lblWho.Text = player1 ? "O" : "X";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            move++;
+            ((Button)sender).Text = player1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+
+            if (move >= 5)
+            {
+                Check();
+            }
+
+            player1 = !player1;     // zmiana gracza po wykonaniu ruchu
+            lblWho.Text = player1 ? "O" : "X";
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            move++;
+            ((Button)sender).Text = player1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+
+            if (move >= 5)
+            {
+                Check();
+            }
+
+            player1 = !player1;     // zmiana gracza po wykonaniu ruchu
+            lblWho.Text = player1 ? "O" : "X";
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            move++;
+            ((Button)sender).Text = player1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+
+            if (move >= 5)
+            {
+                Check();
+            }
+
+            player1 = !player1;     // zmiana gracza po wykonaniu ruchu
+            lblWho.Text = player1 ? "O" : "X";
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            move++;
+            ((Button)sender).Text = player1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+
+            if (move >= 5)
+            {
+                Check();
+            }
+
+            player1 = !player1;     // zmiana gracza po wykonaniu ruchu
+            lblWho.Text = player1 ? "O" : "X";
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            move++;
+            ((Button)sender).Text = player1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+
+            if (move >= 5)
+            {
+                Check();
+            }
+
+            player1 = !player1;     // zmiana gracza po wykonaniu ruchu
+            lblWho.Text = player1 ? "O" : "X";
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            move++;
+            ((Button)sender).Text = player1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+
+            if (move >= 5)
+            {
+                Check();
+            }
+
+            player1 = !player1;     // zmiana gracza po wykonaniu ruchu
+            lblWho.Text = player1 ? "O" : "X";
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            move++;
+            ((Button)sender).Text = player1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+
+            if (move >= 5)
+            {
+                Check();
+            }
+
+            player1 = !player1;     // zmiana gracza po wykonaniu ruchu
+            lblWho.Text = player1 ? "O" : "X";
+
+        }
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            move++;
+            ((Button)sender).Text = player1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+
+            if (move >= 5)
+            {
+                Check();
+            }
+
+            player1 = !player1;     // zmiana gracza po wykonaniu ruchu
+            lblWho.Text = player1 ? "O" : "X";
+
+        }
+
+    }
+}
+
